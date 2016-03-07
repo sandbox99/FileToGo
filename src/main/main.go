3c26d152fbeda2d1f464a6acffdb995cb5ed17ee
unknown <beethoven99@126.com>
2016-03-07 18:06:31 +0800

    function implementation

--- src/main/main.go
+++ src/main/main.go
@@ -3,73 +3,99 @@ package main
 import (
 	"fmt"
 	"io"
-	"net/http"
 	"os"
+	"strings"
+	"net/http"
 )
 
 func main(){
+	
 	http.HandleFunc("/upload",doUpload)
 	http.HandleFunc("/",showUploadPage)
+	http.HandleFunc("/blank",showBlankPage)
 	
 	fmt.Println("Will listen at port 9999")
 	http.ListenAndServe(":9999",nil)
 	
 }
 
-func showUploadPage(res http.ResponseWriter, request *http.Request){
+func readPageContent(res http.ResponseWriter) (int64,error){
+	file,err:=os.Open("resource/index.html")
+	if err!=nil{
+		panic("read file failed.")
+		return 0,err
+	}
+	defer file.Close()
+	return io.Copy(res,file)
 	
 }
 
-func doUpload(res http.ResponseWriter, request *http.Request){
-	fmt.Println("call doUpload...")
+func showUploadPage(res http.ResponseWriter, request *http.Request){
 	res.Header().Set(
 		"Content-Type",
 		"text/html",
 	)
 	
+	readPageContent(res)
+}
+
+func showBlankPage(res http.ResponseWriter, request *http.Request){
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	
+	io.WriteString(res,wrapSimpleHtml(""))
+}
+
+func doUpload(res http.ResponseWriter, request *http.Request){
+	fmt.Println("========call doUpload========")
+	
+	res.Header().Set(
+		"Content-Type",
+		"text/html; charset=utf-8",
+	)
+	
+	if request.Method=="GET"{
+		io.WriteString(res,wrapSimpleHtml("url accepts method POST only"))
+		return
+	}
+	
 	file,header,err:=request.FormFile("file1")
 	if err!=nil{
-		fmt.Println(err)
-		
+		fmt.Println("Error while call FormFile",err)
+		io.WriteString(res,wrapSimpleHtml("发生了错误"))
+		return
 	}else{
-		fmt.Println(header)
+		
 		destfilename:=header.Filename
+		fmt.Println("File name is:",destfilename)
 		destfile,err:=os.Create(destfilename)
 		if err!=nil{
-			fmt.Println("create file failed:",err,destfilename)
-			io.WriteString(res,"bad occurred")
+			fmt.Println("create file failed:", err, destfilename)
+			io.WriteString(res,wrapSimpleHtml("发生了错误:"+err.Error()))
 			return
 		}
 		defer destfile.Close()
 		cnt,err:=io.Copy(destfile,file)
 		if err!=nil{
-			fmt.Println("Write file failed:",err)
-			io.WriteString(res,"bad occurred")
+			fmt.Println("Error while call Copy:",err)
+			io.WriteString(res,wrapSimpleHtml("发生了错误"))
 			return
 		}
-		fmt.Println("Written ",cnt)
-		
+		fmt.Println("Written",cnt,"bytes")
+		io.WriteString(res,wrapSimpleHtml(fmt.Sprintf("成功(%d bytes)",cnt)))
 	}
-	
-	io.WriteString(res,
-		`<DOCTYPE html>
-<html>
-	<head>
-		<title>Hello</title>
-	</head>
-	<body>
-		Hello World!
-		<form action="upload" method="POST" target="fresult" enctype="multipart/form-data">
-			<input type="file" name="file1">
-			<input type="submit">
-		</form>
-		<iframe id="fresult"></iframe>
-	</body>
-</html>`,
-	)
 }
 
-
-
-
-
+func wrapSimpleHtml(str string)string{
+	var template string=`<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<style type="text/css">body{font-family:Microsoft Yahei;}</style>
+	</head>
+	<body>MSG</body>
+</html>`
+	return strings.Replace(template,"MSG",str,1)
+}
